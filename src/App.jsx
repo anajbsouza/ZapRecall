cb680b2498f2d733f31b48c0668f25f7d1ea40d2
anajbsouza <anaju.batsouza@gmail.com>
2023-05-19 22:26:18 -0300

    styled-components added with yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import HomePage from "./components/HomePage";
 import Deck from "./components/Deck";
+import GlobalStyles from "./GlobalStyles";
+
 
 function App() {
   const [welcome, setWelcome] = React.useState(true);
   return (
     <>
+      <GlobalStyles />
       {welcome ? <HomePage setWelcome={setWelcome} /> : <Deck />}
     </>
   );
